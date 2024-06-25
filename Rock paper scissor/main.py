ce3d28ffd18a46a245531f27b1ec5f3e5730b7f4
Pavansomisetty21 <110320361+Pavansomisetty21@users.noreply.github.com>
2024-06-25 12:24:00 +0530

    Update main.py

--- Rock paper scissor/main.py
+++ Rock paper scissor/main.py
@@ -1,1 +1,23 @@
+# This entrypoint file to be used in development. Start by reading README.md
+from RPS_game import play, pavan, abbey, quincy, sanju, human, random_player
+from RPS import player
+from unittest import main
 
+#play(player, quincy, 1000)
+#play(player, abbey, 1000)
+#play(player, sanju, 1000)
+#play(player, Pavan, 1000)
+
+
+
+
+# Uncomment line below to play interactively against a bot:
+# play(human, abbey, 20, verbose=True)
+
+# Uncomment line below to play against a bot that plays randomly:
+# play(human, random_player, 1000)
+
+
+
+# Uncomment line below to run unit tests automatically
+main(module='test_module', exit=False)
